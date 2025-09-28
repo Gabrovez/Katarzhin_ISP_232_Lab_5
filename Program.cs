cd078d0e90bc83d8bae96f73e1cbf01bc673de24
Gabrovez <gabrovez321@gmail.com>
2025-09-28 22:44:10 +0300

    Исправил задчу про игру + приветствие на выбранном языке:

--- Program.cs
+++ Program.cs
@@ -3,10 +3,29 @@
     class Program
     {
         //static void SayHello() => Console.WriteLine("Hello");
-        static void Game() => Console.WriteLine("Limbus Company");
+        static void Game()
+        {
+            string mGame = "Limbus Company";
+            Console.WriteLine(mGame);
+        }
         static void Main()
         {
             Game();
+            Console.WriteLine("Введите своё имя: ");
+            string? name = Console.ReadLine();
+            Console.Write("Введите ваш язык(русский, английский, немецкий): ");
+            string language = Console.ReadLine()!.ToLower();
+
+            void SayHelloRu() => Console.WriteLine($"Привет, {name}!");
+            void SayHelloEn() => Console.WriteLine($"Hello, {name}!");
+            void SayHelloDe() => Console.WriteLine($"Guten Tag, {name}!");
+            switch (language)
+            {
+                case "английский": SayHelloEn(); break;
+                case "русский": SayHelloRu(); break;
+                case "немецкий": SayHelloDe(); break;
+                default: Console.WriteLine("Язык не найден!"); break;
+            }
         }
     }
 }
