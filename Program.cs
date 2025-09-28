a726256ad525d3219f94860295c315f95425cb5a
Gabrovez <gabrovez321@gmail.com>
2025-09-29 00:55:56 +0300

    Метод print

--- Program.cs
+++ Program.cs
@@ -3,29 +3,38 @@
     class Program
     {
         //static void SayHello() => Console.WriteLine("Hello");
-        static void Game()
-        {
-            string mGame = "Limbus Company";
-            Console.WriteLine(mGame);
-        }
+        //static void Game()
+        //{
+        //    string mGame = "Limbus Company";
+        //    Console.WriteLine(mGame);
+        //}
+
         static void Main()
         {
-            Game();
-            Console.WriteLine("Введите своё имя: ");
-            string? name = Console.ReadLine();
-            Console.Write("Введите ваш язык(русский, английский, немецкий): ");
-            string language = Console.ReadLine()!.ToLower();
-
-            void SayHelloRu() => Console.WriteLine($"Привет, {name}!");
-            void SayHelloEn() => Console.WriteLine($"Hello, {name}!");
-            void SayHelloDe() => Console.WriteLine($"Guten Tag, {name}!");
-            switch (language)
+            void print(string message)
             {
-                case "английский": SayHelloEn(); break;
-                case "русский": SayHelloRu(); break;
-                case "немецкий": SayHelloDe(); break;
-                default: Console.WriteLine("Язык не найден!"); break;
+                Console.WriteLine(message);
             }
+            Console.WriteLine("Как тебя зовут?");
+            Console.Write("Введите своё имя: ");
+            string? name2 = Console.ReadLine();
+            print(message: $"Твоё имя: {name2}");
+            //Game();
+            //Console.WriteLine("Введите своё имя: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Введите ваш язык(русский, английский, немецкий): ");
+            //string language = Console.ReadLine()!.ToLower();
+
+            //void SayHelloRu() => Console.WriteLine($"Привет, {name}!");
+            //void SayHelloEn() => Console.WriteLine($"Hello, {name}!");
+            //void SayHelloDe() => Console.WriteLine($"Guten Tag, {name}!");
+            //switch (language)
+            //{
+            //    case "английский": SayHelloEn(); break;
+            //    case "русский": SayHelloRu(); break;
+            //    case "немецкий": SayHelloDe(); break;
+            //    default: Console.WriteLine("Язык не найден!"); break;
+            //}
         }
     }
 }
