a3e44cc8b1ba0f184804bd06ffe6891d0fa72ff9
Gabrovez <gabrovez321@gmail.com>
2025-09-29 03:44:46 +0300

    Статические локальные функции

--- Program.cs
+++ Program.cs
@@ -126,28 +126,42 @@
             //Console.WriteLine($"Факториал числа 4 = {factorial}");
             //Console.WriteLine($"Факториал числа 5 = {factorial2}");
             //Console.WriteLine($"Факториал числа 6 = {factorial3}");
-            void Compare(int[] numbers1, int[] numbers2)
-            {
-                int numbers1Sum = Sum(numbers1);
-                int numbers2Sum = Sum(numbers2);
-                if (numbers1Sum > numbers2Sum)
-                    Console.WriteLine("cywwa чисел из массива numbers1 больше");
-                else if (numbers1Sum < numbers2Sum)
-                    Console.WriteLine("cywwa чисел из массива numbers2 больше");
-                else
-                    Console.WriteLine("cywwa чисел обоих массивов равны");
-                int Sum(int[] numbers)
-                {
-                    int res = 0;
-                    foreach (int num in numbers)
-                        res += num;
-                    return res;
+            //void Compare(int[] numbers1, int[] numbers2)
+            //{
+            //    int numbers1Sum = Sum(numbers1);
+            //    int numbers2Sum = Sum(numbers2);
+            //    if (numbers1Sum > numbers2Sum)
+            //        Console.WriteLine("cywwa чисел из массива numbers1 больше");
+            //    else if (numbers1Sum < numbers2Sum)
+            //        Console.WriteLine("cywwa чисел из массива numbers2 больше");
+            //    else
+            //        Console.WriteLine("cywwa чисел обоих массивов равны");
+            //    int Sum(int[] numbers)
+            //    {
+            //        int res = 0;
+            //        foreach (int num in numbers)
+            //            res += num;
+            //        return res;
+            //    }
+            //}
+            //int[] numbers1 = [1, 2, 3];
+            //int[] numbers2 = [3, 4, 5, 6, 7];
+            //Compare(numbers1, numbers2);
+            int Sum(int[] numbers){
+                int limit = 0;
+                int result = 0;
+                bool IsPassed(int number){
+                    return number > limit;
                 }
+                foreach (int number in numbers) {
+                    if (IsPassed(number)) result += number;
+                }
+                return result;
             }
-            int[] numbers1 = [1, 2, 3];
-            int[] numbers2 = [3, 4, 5, 6, 7];
-            Compare(numbers1, numbers2);
-
+            int[] numbers1 = [-3, -2, -1, 0, 1, 2, 3];
+            int[] numbers2 = [3, -4, 5, -6, 7];
+            Console.WriteLine(Sum(numbers1));
+            Console.WriteLine(Sum(numbers2));
 
         }
     }
